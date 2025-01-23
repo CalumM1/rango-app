7650cfc6d03aa24364dee3ab5c8789e39571b100
Calum McCarthy <2655113m@student.gla.ac.uk>
2025-01-23 17:16:49 +0000

    fix urls

--- rango/views.py
+++ rango/views.py
@@ -5,4 +5,4 @@ def index(request):
     return HttpResponse("Welcome to the index page! Click <a href='/rango/about/'> HERE </a> to go to about.")
 
 def about(request):
-    return HttpResponse("Welcome to the about page! Click <a href='http:/rango/'> HERE </a> to go to the index page.")
+    return HttpResponse("Welcome to the about page! Click <a href='/rango/'> HERE </a> to go to the index page.")
