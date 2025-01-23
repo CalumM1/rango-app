a50f717ac5c7102d8f38eefe00f5d3115f1039e5
Calum McCarthy <2655113m@student.gla.ac.uk>
2025-01-23 16:54:45 +0000

    added about page in rango app and created links betweeen the two pages

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse("Welcome to the index page! Click <a href='http://127.0.0.1:8000/rango/about/'> HERE </a> to go to about.")
+
+def about(request):
+    return HttpResponse("Welcome to the about page! Click <a href='http://127.0.0.1:8000/rango/'> HERE </a> to go to the index page.")
