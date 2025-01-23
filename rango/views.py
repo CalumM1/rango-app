ae99056eb8c7aa9d9dbe6f51e2940d5dc353941d
Calum McCarthy <2655113m@student.gla.ac.uk>
2025-01-23 17:44:43 +0000

    update index view to use the template

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Welcome to the index page! Click <a href='/rango/about/'> HERE </a> to go to about.")
+    # construct dictionary to pass to template engine
+    context_dict = {'boldmessage': 'crunchy, creamy, cookie, candy, cupcake!'}
+
+    # run rendered response to send to client
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     return HttpResponse("Welcome to the about page! Click <a href='/rango/'> HERE </a> to go to the index page.")
