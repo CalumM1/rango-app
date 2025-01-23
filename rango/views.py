ab8c7d8c2f2b66ff4549cb1d2d482e46e7f6c11d
Calum McCarthy <2655113m@student.gla.ac.uk>
2025-01-23 17:12:35 +0000

    add chapter 3 tests

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Welcome to the index page! Click <a href='http://127.0.0.1:8000/rango/about/'> HERE </a> to go to about.")
+    return HttpResponse("Welcome to the index page! Click <a href='/rango/about/'> HERE </a> to go to about.")
 
 def about(request):
-    return HttpResponse("Welcome to the about page! Click <a href='http://127.0.0.1:8000/rango/'> HERE </a> to go to the index page.")
+    return HttpResponse("Welcome to the about page! Click <a href='http:/rango/'> HERE </a> to go to the index page.")
